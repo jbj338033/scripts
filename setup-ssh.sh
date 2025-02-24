e4e0c015ac9cd29fa2337abf2420d817c5bdd2e2
전민오 <jbj338033@gmail.com>
2025-02-24 19:27:59 +0900

    feat: setup ssh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-apt update
-apt install -y openssh-server
+sudo apt update
+sudo apt install -y openssh-server
 
-systemctl start ssh
-systemctl enable ssh
+sudo systemctl start ssh
+sudo systemctl enable ssh
 
-ufw allow ssh
-ufw --force enable
+sudo ufw allow ssh
+sudo ufw --force enable
 
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
+sudo mkdir -p ~/.ssh
+sudo chmod 700 ~/.ssh
 
-ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
-chown -R ubuntu:ubuntu ~/.ssh
+sudo ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
+sudo cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+sudo chmod 600 ~/.ssh/authorized_keys
+sudo chown -R ubuntu:ubuntu ~/.ssh
 
 cat ~/.ssh/id_rsa
