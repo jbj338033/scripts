a304b08ac51b8a24cf75d9ed8c492f73ea639902
전민오 <jbj338033@gmail.com>
2025-02-24 18:47:35 +0900

    feat: setup-ssh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+apt install -y openssh-server
+systemctl start ssh
+systemctl enable ssh
+
+ufw allow ssh
+
 mkdir -p ~/.ssh
 ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
